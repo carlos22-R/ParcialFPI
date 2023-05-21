19b535f517d818daf07caf2ac90dab42225129ca
carlos22-R <rc19102@ues.edu.sv>
2023-05-21 02:06:11 -0600

    habilitar el cors

---
+++ src/main/java/tpi135_2023/ingenieria/occ/ues/edu/sv/Delivery/boundary/CorsFilter.java
@@ -0,0 +1,25 @@
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class CorsFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        // Configurar los encabezados CORS
+        httpResponse.setHeader("Access-Control-Allow-Origin", "*");
+        httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+        httpResponse.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+        // Continuar con la cadena de filtros
+        chain.doFilter(request, response);
+    }
+
+    // Métodos init() y destroy() pueden permanecer vacíos o implementarse según tus necesidades.
+}
